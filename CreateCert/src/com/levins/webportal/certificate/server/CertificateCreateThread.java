d0fddb646a0e76ae9c5b4a44d77695ad596ed7c3
Krachunov <Krachunov@Krachunov-PC>
2015-12-21 14:44:04 +0200

    Move internal class into normal class

---
+++ CreateCert/src/com/levins/webportal/certificate/server/CertificateCreateThread.java
@@ -0,0 +1,52 @@
+package com.levins.webportal.certificate.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+import com.levins.webportal.certificate.data.CertificateInfo;
+
+class CertificateCreateThread extends Thread {
+	private int CLIENT_REQUEST_TIMEOUT = 15 * 60 * 1000; // 15 min.
+	private Socket connection;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private CreateNewBatFile batGenerator;
+
+	public CertificateCreateThread(Socket connection) throws IOException {
+		this.connection = connection;
+		this.connection.setSoTimeout(CLIENT_REQUEST_TIMEOUT);
+		in = new DataInputStream(connection.getInputStream());
+		out = new DataOutputStream(connection.getOutputStream());
+		batGenerator = new CreateNewBatFile();
+	}
+
+	public void run() {
+		try {
+
+			out.writeUTF(CreateCertServer.GREETING_MESSAGE_TO_CLIENT);
+			out.flush();
+			while (!isInterrupted()) {
+				String input = in.readUTF();
+
+				CertificateInfo certificate = batGenerator.generateCert(input);
+				CreateCertServer.getCertificationList().add(certificate);
+
+				String statusRequest = "You send to server "
+						+ certificate.toString()
+						+ " and thread who done is this its " + getName();
+				out.writeUTF(String.format(statusRequest));
+				out.flush();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		// TODO save xml file with new users
+
+		System.out.printf("%s : Connection lost  : %s:%s\n", new Date(),
+				connection.getInetAddress().getHostAddress(),
+				connection.getPort());
+	}
+}
--- CreateCert/src/com/levins/webportal/certificate/server/CreateCertServer.java
+++ CreateCert/src/com/levins/webportal/certificate/server/CreateCertServer.java
@@ -1,13 +1,10 @@
 package com.levins.webportal.certificate.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.xml.bind.JAXB;
@@ -45,50 +42,9 @@ public class CreateCertServer {
 		return certificationList;
 	}
 
-	public static void setCertificationList(List<CertificateInfo> certificationList) {
+	public static void setCertificationList(
+			List<CertificateInfo> certificationList) {
 		CreateCertServer.certificationList = certificationList;
 	}
 
-	static class CertificateCreateThread extends Thread {
-		private int CLIENT_REQUEST_TIMEOUT = 15 * 60 * 1000; // 15 min.
-		private Socket connection;
-		private DataInputStream in;
-		private DataOutputStream out;
-		private CreateNewBatFile batGenerator;
-
-		public CertificateCreateThread(Socket connection) throws IOException {
-			this.connection = connection;
-			this.connection.setSoTimeout(CLIENT_REQUEST_TIMEOUT);
-			in = new DataInputStream(connection.getInputStream());
-			out = new DataOutputStream(connection.getOutputStream());
-			batGenerator = new CreateNewBatFile();
-		}
-
-		public void run() {
-			try {
-
-				out.writeUTF(GREETING_MESSAGE_TO_CLIENT);
-				out.flush();
-				while (!isInterrupted()) {
-					String input = in.readUTF();
-
-					CertificateInfo certificate = batGenerator.generateCert(input);
-					CreateCertServer.getCertificationList().add(certificate);
-
-					String statusRequest = "You send to server "
-							+ certificate.toString()
-							+ " and thread who done is this its " + getName();
-					out.writeUTF(String.format(statusRequest));
-					out.flush();
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			// TODO save xml file with new users
-
-			System.out.printf("%s : Connection lost  : %s:%s\n", new Date(),
-					connection.getInetAddress().getHostAddress(),
-					connection.getPort());
-		}
-	}
 }
