c2875d8227678c8ea6cc5a907f8b906f33049d0c
Krachunov <Krachunov@Krachunov-PC>
2015-12-21 16:58:06 +0200

    Mail sender works

--- CreateCert/src/com/levins/webportal/certificate/client/MailSender.java
+++ CreateCert/src/com/levins/webportal/certificate/client/MailSender.java
@@ -1,57 +1,67 @@
 package com.levins.webportal.certificate.client;
 
+import java.io.UnsupportedEncodingException;
 import java.util.*;
+
 import javax.mail.*;
 import javax.mail.internet.*;
-import javax.activation.*;
 
 public class MailSender {
 
-	public static void main(String [] args)
-	   {
-	      
-	      // Recipient's email ID needs to be mentioned.
-	      String to = "krachunov@lev-ins.com";
+	public static void main(String[] args) throws UnsupportedEncodingException,
+			MessagingException {
 
-	      // Sender's email ID needs to be mentioned
-	      String from = "krachunov@lev-ins.com";
+		// Recipient's email ID needs to be mentioned.
+		String to = "krachunov@lev-ins.com";
 
-	      // Assuming you are sending email from localhost
-	      String host = "mail.lev-ins.com";
+		// Sender's email ID needs to be mentioned
+		String from = "krachunov@lev-ins.com";
 
-	      // Get system properties
-	      Properties properties = System.getProperties();
+		// Assuming you are sending email from localhost
+		String host = "mail.lev-ins.com";
 
-	      // Setup mail server
-	      properties.setProperty("mail.smtp.host", host);
-	      properties.setProperty("mail.user", "krachunov");
-	      properties.setProperty("mail.password", "Cipokrilo");
+		// Get system properties
+		Properties properties = System.getProperties();
 
-	      // Get the default Session object.
-	      Session session = Session.getDefaultInstance(properties);
+		// Setup mail server
+		properties.setProperty("mail.smtp.host", host);
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.imap.starttls.enable", "true");
 
-	      try{
-	         // Create a default MimeMessage object.
-	         MimeMessage message = new MimeMessage(session);
+		// Get the default Session object.
+		// Session session = Session.getDefaultInstance(properties);
 
-	         // Set From: header field of the header.
-	         message.setFrom(new InternetAddress(from));
+		Session session = Session.getDefaultInstance(properties,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("krachunov",
+								"Cipokrilo");
+					}
+				});
 
-	         // Set To: header field of the header.
-	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+		try {
+			// Create a default MimeMessage object.
+			MimeMessage message = new MimeMessage(session);
 
-	         // Set Subject: header field
-	         message.setSubject("This is the Subject Line!");
+			// Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
 
-	         // Send the actual HTML message, as big as you like
-	         message.setContent("<h1>This is actual message</h1>", "text/html" );
+			// Set To: header field of the header.
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+					to));
 
-	         // Send message
-	         Transport.send(message);
-	         System.out.println("Sent message successfully....");
-	      }catch (MessagingException mex) {
-	         mex.printStackTrace();
-	      }
-	   }
-	
+			// Set Subject: header field
+			message.setSubject("This is the Subject Line!");
+
+			// Send the actual HTML message, as big as you like
+			message.setContent("<h1>This is actual message</h1>", "text/html");
+
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+
 }
