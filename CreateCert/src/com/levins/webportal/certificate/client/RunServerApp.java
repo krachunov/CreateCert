8169204ee4fed4b7bc27c5126573c98f9d342e8f
ico <ico@192.168.8.54>
2015-12-27 10:41:27 +0200

    Rename main class

--- CreateCert/src/com/levins/webportal/certificate/client/RunServerApp.java
+++ CreateCert/src/com/levins/webportal/certificate/client/RunServerApp.java
@@ -1,6 +1,6 @@
 package com.levins.webportal.certificate.client;
 
-public class TestClientApp {
+public class RunServerApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
