24bda18b9d1e7b4af48bfb97a2620ab75127a88f
Krachunov <Krachunov@Krachunov-PC>
2015-12-30 13:08:27 +0200

    Remove warnings

--- CreateCert/src/com/levins/webportal/certificate/client/UI/ClientPanel.java
+++ CreateCert/src/com/levins/webportal/certificate/client/UI/ClientPanel.java
@@ -31,14 +31,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JCheckBox;
 
 @SuppressWarnings("serial")
 public class ClientPanel extends JFrame implements Serializable {
-	private static final String FILE_TO_LOAD_SETTINGS = "resources/clientSetings";
-	private static final String PATH_LOGO = "resources\\levins.jpg";
+	private static final String FILE_TO_LOAD_SETTINGS = "clientSetings";
+	 private static final String PATH_LOGO = "resources\\levins.jpg";
 
 	private Map<String, Object> restorSettings;
 	private JTextField userNameTextField;
@@ -119,21 +118,24 @@ public class ClientPanel extends JFrame implements Serializable {
 		gbc_passwordTextField.gridy = 2;
 		getContentPane().add(passwordTextField, gbc_passwordTextField);
 
-		BufferedImage myPicture = null;
-		try {
-			myPicture = ImageIO.read(new File(PATH_LOGO));
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		// TODO
+//		 BufferedImage myPicture = null;
+//		 try {
+//		 myPicture = ImageIO.read(new File(PATH_LOGO));
+//		 } catch (IOException e1) {
+//		 e1.printStackTrace();
+//		 }
+//
+//		 JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+//		 GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+//		 gbc_lblNewLabel.gridheight = 3;
+//		 gbc_lblNewLabel.gridwidth = 2;
+//		 gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+//		 gbc_lblNewLabel.gridx = 2;
+//		 gbc_lblNewLabel.gridy = 2;
+//		 getContentPane().add(picLabel, gbc_lblNewLabel);
 
-		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.gridheight = 3;
-		gbc_lblNewLabel.gridwidth = 2;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 2;
-		gbc_lblNewLabel.gridy = 2;
-		getContentPane().add(picLabel, gbc_lblNewLabel);
+
 
 		JLabel lblServerAddress = new JLabel("Server address");
 		GridBagConstraints gbc_lblServerAddress = new GridBagConstraints();
