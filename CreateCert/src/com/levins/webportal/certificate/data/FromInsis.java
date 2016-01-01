9ebd979bf70f00df8dc52a69850c6a7775d1ed87
ico <ico@192.168.8.109>
2016-01-01 16:01:29 +0200

    Connect to oracle DB

---
+++ CreateCert/src/com/levins/webportal/certificate/data/FromInsis.java
@@ -0,0 +1,45 @@
+package com.levins.webportal.certificate.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class FromInsis {
+
+	static String query = "select * from policy p where p.username like 'W00088061_13'";
+
+	public static void main(String[] args) throws SQLException {
+
+		// URL of Oracle database server
+		String url = "jdbc:oracle:thin:@172.20.10.8:1521:INSISDB";
+	
+
+		// properties for creating connection to Oracle database
+		Properties props = new Properties();
+		props.setProperty("user", "s0000");
+		props.setProperty("password", "r3s3rv3");
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		// creating connection to Oracle database using JDBC
+		Connection conn = DriverManager.getConnection(url, props);
+
+		String sql = "select sysdate as current_day from dual";
+
+		// creating PreparedStatement object to execute query
+		PreparedStatement preStatement = conn.prepareStatement(query);
+
+		ResultSet result = preStatement.executeQuery();
+
+		while (result.next()) {
+			System.out.println("RESULT : "
+					+ result.getString("CLIENT_ID"));
+		}
+		System.out.println("done");
+	}
+}
