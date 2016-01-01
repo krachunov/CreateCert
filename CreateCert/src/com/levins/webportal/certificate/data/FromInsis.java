bed677e30a5059e73955f287973380dfe2601db2
ico <ico@192.168.8.109>
2016-01-01 17:08:37 +0200

    Format method who search and print user from database

--- CreateCert/src/com/levins/webportal/certificate/data/FromInsis.java
+++ CreateCert/src/com/levins/webportal/certificate/data/FromInsis.java
@@ -9,18 +9,27 @@ import java.util.Properties;
 
 public class FromInsis {
 
-	static String query = "select * from policy p where p.username like 'W00088061_13'";
-	static String queryPortal = "Select d.username from dba_users d where d.username like 'W19029015_%'";
-
 	public static void main(String[] args) throws SQLException {
 
+		String findUser = "W0008%";
+		String user = "insis";
+		String pass = "change2015";
+		printDataBaseResult(findUser, user, pass);
+		System.out.println("done");
+	}
+
+	private static void printDataBaseResult(String userDataBase,
+			String passwordDataBase, String findingName) throws SQLException {
+		String queryPortal = String
+				.format("Select d.username,(select pp.name from p_people pp, p_staff ps where ps.man_id=pp.man_id and ps.security_id=d.username) »Ã≈,(select pp1.egn from p_people pp1, p_staff ps1 where ps1.man_id=pp1.man_id and ps1.security_id=d.username) EGN,(select ps.user_email from p_people pp, p_staff ps where ps.man_id=pp.man_id and ps.security_id=d.username) EMAIL from dba_users d where d.username like '%s'",
+						findingName);
 		// URL of Oracle database server
 		String url = "jdbc:oracle:thin:@172.20.10.8:1521:INSISDB";
 
 		// properties for creating connection to Oracle database
 		Properties props = new Properties();
-		props.setProperty("user", "s0000");
-		props.setProperty("password", "r3s3rv3");
+		props.setProperty("user", userDataBase);
+		props.setProperty("password", passwordDataBase);
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 		} catch (ClassNotFoundException e) {
@@ -32,14 +41,21 @@ public class FromInsis {
 		String sql = "'select sysdate as current_day from dual";
 
 		// creating PreparedStatement object to execute query
-		PreparedStatement preStatement = conn.prepareStatement(query);
+		PreparedStatement preStatement = conn.prepareStatement(queryPortal);
 
 		ResultSet result = preStatement.executeQuery();
 
 		while (result.next()) {
-			System.out.println("RESULT : " + result.getString("USERNAME"));
+			printResult(result);
 
 		}
-		System.out.println("done");
+	}
+
+	private static void printResult(ResultSet result) throws SQLException {
+		final String userName = result.getString("USERNAME");
+		final String name = result.getString("»Ã≈");
+		final String mail = result.getString("EMAIL");
+		System.out.printf("User Portal:%s Name: %s - mail: %s  \n", userName,
+				name, mail);
 	}
 }
