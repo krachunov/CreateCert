fadb56e9ea110d3b9e2b808722f1247f7a3d36a3
ico <ico@192.168.8.127>
2016-01-10 15:48:51 +0200

    Table model

--- CreateCert/src/com/levins/webportal/certificate/client/UI/searchTable/TableModel.java
+++ CreateCert/src/com/levins/webportal/certificate/client/UI/searchTable/TableModel.java
@@ -1,60 +1,60 @@
-//package com.levins.webportal.certificate.client.UI.searchTable;
-//import java.util.List;
-//import java.util.Queue;
-//
-//import javax.swing.table.AbstractTableModel;
-//
-//@SuppressWarnings("serial")
-//public class TableModel extends AbstractTableModel {
-//	private static final int COLUMNS_COUNT = 7;
-//	private List<Animal> listToTable;
-//
-//	public List<Animal> getListToTable() {
-//		return listToTable;
-//	}
-//
-//	public void setListToTable(List<Animal> listToTable) {
-//		this.listToTable = listToTable;
-//		fireTableDataChanged();
-//	}
-//
-//	@Override
-//	public int getColumnCount() {
-//		return COLUMNS_COUNT;
-//	}
-//
-//	@Override
-//	public int getRowCount() {
-//
-//		return (listToTable != null ? listToTable.size() : 0);
-//	}
-//
-//	@Override
-//	public Object getValueAt(int rowIndex, int columnIndex) {
-//		List list = getListToTable();
-//		Animal singleAnimal = listToTable.get(rowIndex);
-//
-//		switch (columnIndex) {
-//		case 0:
-//			return singleAnimal.getDate();
-//		case 1:
-//			return singleAnimal.getColor();
-//		case 2:
-//			return singleAnimal.getBreed();
-//		case 3:
-//			return singleAnimal.getGender();
-//		case 4:
-//			return singleAnimal.getName();
-//		case 5:
-//			return singleAnimal.getEntry();
-//		case 6:
-//			return singleAnimal.getDate();
-//		}
-//		return null;
-//	}
-//
-//	public void deleteRecord(int index) {
-//		listToTable.remove(index);
-//		fireTableDataChanged();
-//	}
-//}
+package com.levins.webportal.certificate.client.UI.searchTable;
+
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.levins.webportal.certificate.data.CertificateInfo;
+
+@SuppressWarnings("serial")
+public class TableModel extends AbstractTableModel {
+	private static final int COLUMNS_COUNT = 7;
+	private List<CertificateInfo> listToTable;
+
+	public List<CertificateInfo> getListToTable() {
+		return listToTable;
+	}
+
+	public void setListToTable(List<CertificateInfo> listToTable) {
+		this.listToTable = listToTable;
+		fireTableDataChanged();
+	}
+
+	public int getColumnCount() {
+		return COLUMNS_COUNT;
+	}
+
+	public int getRowCount() {
+
+		return (listToTable != null ? listToTable.size() : 0);
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		List list = getListToTable();
+		CertificateInfo singleCert = listToTable.get(rowIndex);
+
+		switch (columnIndex) {
+		case 0:
+			return singleCert.getUserName();
+		case 1:
+			return singleCert.getFirstName();
+		case 2:
+			return singleCert.getLastName();
+		case 3:
+			return singleCert.getPassword();
+		case 4:
+			return singleCert.getEmail();
+		case 5:
+			return singleCert.getPathToCertificateFile();
+		case 6:
+			return singleCert.getEgn();
+		}
+		return null;
+	}
+
+	public void deleteRecord(int index) {
+		listToTable.remove(index);
+		fireTableDataChanged();
+	}
+}
