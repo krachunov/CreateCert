f3751e44698a15cfd3b2f8ac1370a9c8c85c1e76
ico <ico@192.168.8.163>
2016-02-12 22:25:25 +0200

    Add method into ErroLog class, need to implement saver in FromInsisData.class

--- CreateCert/src/com/levins/webportal/certificate/data/ErrorLog.java
+++ CreateCert/src/com/levins/webportal/certificate/data/ErrorLog.java
@@ -5,12 +5,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
+import java.util.List;
+
+import com.levins.webportal.certificate.client.UI.ClientPanel;
 
 public class ErrorLog {
+	private static final String NEW_LINE_SEPARATOR = "\n";
 	public static final String ERROR_LOG_FILE_NAME = "errorLog.log";
 	public static final String SKIPPED_USERS_LOG_FILE_NAME = "skippedUserLog.log";
 
-	public static Writer createLogFile(Exception e,String errorLogFileName) {
+	public static Writer createLogFile(Exception e, String errorLogFileName) {
 		Writer writer = null;
 		try {
 			writer = new FileWriter(errorLogFileName, true);
@@ -19,5 +23,52 @@ public class ErrorLog {
 		}
 		e.printStackTrace(new PrintWriter(new BufferedWriter(writer), true));
 		return writer;
+	}
+
+	// TODO
+	public static void createSkippedUsersLog(String fileName,
+			List<String> skippedUsers) {
+
+		String FILE_HEADER = "user;firstName;lastName;mail;password;path;EGN";
+		FileWriter fileWriter = null;
+
+		try {
+			if (!DataValidator.chekFileExist(fileName)) {
+				fileWriter = addHeader(fileName, FILE_HEADER);
+			} else {
+				fileWriter = new FileWriter(fileName, true);
+			}
+			for (String currentRecord : skippedUsers) {
+				addNewRecordInFile(fileWriter, currentRecord);
+
+			}
+		} catch (IOException e) {
+			ClientPanel.popUpMessageException(e, "Error in CsvFileWriterr");
+			e.printStackTrace();
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+			} catch (IOException e) {
+				ClientPanel.popUpMessageException(e,
+						"Error while flushing/closing fileWriter");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static FileWriter addHeader(String fileName, String FILE_HEADER)
+			throws IOException {
+		FileWriter fileWriter;
+		fileWriter = new FileWriter(fileName);
+		fileWriter.append(FILE_HEADER.toString());
+		fileWriter.append(NEW_LINE_SEPARATOR);
+		return fileWriter;
+	}
+
+	private static void addNewRecordInFile(FileWriter fileWriter,
+			String currentRecord) throws IOException {
+		fileWriter.append(currentRecord);
+		fileWriter.append(NEW_LINE_SEPARATOR);
 	}
 }
